a1ff6a721dafcf7309c0e79081fc5e45a8206a37
Ashton Christie <ashchristie@gmail.com>
2017-10-01 13:44:05 -0700

    added game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//defining all 4 cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//log to console what card the user picked
+console.log("User flipped " + cardOne);
