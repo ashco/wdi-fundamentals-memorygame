ca90e55bba2511ea3c0980c2633cb22379ec9b72
Ashton Christie <ashchristie@gmail.com>
2017-10-15 21:35:39 -0700

    cleaned up game logic

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
-//defining all 4 cards in an array
+//defining all 4 cards array
 var cards = ["queen", "queen", "king", "king"];
 
-//array to hold the picked cards
+//array to hold picked cards
 var cardsInPlay = [];
 
-//First picked card
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-//adds picked cards to cardsInPlay
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-//logic for two card picks
-if (cardsInPlay.length === 2){
-//alerts for match/no match
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert ("You found a match!");
-	} else {
-		alert ("Sorry, try again.");
+//flipCard function
+var flipCard = function(cardID) {
+	//adds picked cards to cardsInPlay
+	cardsInPlay.push(cards[cardID]);
+	//logic for two card picks
+	if (cardsInPlay.length === 2){
+	//alerts for match/no match
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert ("You found a match!");
+		} else {
+			alert ("Sorry, try again.");
+		}
 	}
+	//displays picked cards in console
+	console.log("User flipped " + cards[cardID]);
 }
 
-
-
-
-//displays picked cards in console
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(1);
