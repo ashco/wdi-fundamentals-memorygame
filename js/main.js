1bcf563f92e083c1848836b7121b81f5df097c54
Ashton Christie <ashchristie@gmail.com>
2017-10-30 00:04:27 -0700

    added objects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 //defining all 4 cards array
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}
+	];
 
 //array to hold picked cards
 var cardsInPlay = [];
@@ -7,7 +28,7 @@ var cardsInPlay = [];
 //flipCard function
 var flipCard = function(cardID) {
 	//adds picked cards to cardsInPlay
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
 	//logic for two card picks
 	if (cardsInPlay.length === 2){
 	//alerts for match/no match
@@ -18,8 +39,10 @@ var flipCard = function(cardID) {
 		}
 	}
 	//displays picked cards in console
-	console.log("User flipped " + cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(2);
