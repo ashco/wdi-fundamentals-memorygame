1743a1d74e52a5c82ef7e6b5598d9e1003208287
Ashton Christie <ashchristie@gmail.com>
2017-10-31 20:02:25 -0700

    working game

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//defining all 4 cards array
+//defining all 4 cards array w/ objects
 var cards = [
 	{
 	rank: "queen",
@@ -26,9 +26,14 @@ var cards = [
 var cardsInPlay = [];
 
 //flipCard function
-var flipCard = function(cardID) {
-	//adds picked cards to cardsInPlay
-	cardsInPlay.push(cards[cardID].rank);
+var flipCard = function() {
+	//takes data-id value, sets number to cardId variable
+	var cardId = this.getAttribute('data-id');
+	//changes image of selected card to .cardImage
+	this.setAttribute('src', cards[cardId].cardImage);
+	//adds picked cards to cardsInPlay variable
+	cardsInPlay.push(cards[cardId].rank);
+
 	//logic for two card picks
 	if (cardsInPlay.length === 2){
 	//alerts for match/no match
@@ -38,11 +43,24 @@ var flipCard = function(cardID) {
 			alert ("Sorry, try again.");
 		}
 	}
-	//displays picked cards in console
-	console.log("User flipped " + cards[cardID].rank);
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
 }
 
-flipCard(0);
-flipCard(2);
+//function that creates cards to flip
+var createBoard = function() {
+	//for loop loops 4 times, creates 4 cards
+	for (var i = 0; i < cards.length; i++) {
+		//each loop creates var cardElement with img
+		var cardElement = document.createElement('img');
+		//adds attribute back image
+		cardElement.setAttribute('src', 'images/back.png');
+		//adds data-id for each cardElement
+		cardElement.setAttribute('data-id', i);
+		//adds event listener to run flipCard function on click
+		cardElement.addEventListener('click', flipCard);
+		//inserts cardElement as child to game-board ID in HTML
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+//call function createBoard to start it up
+createBoard()
